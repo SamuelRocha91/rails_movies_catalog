5dda1d593785771bd0c0152dbbc9caec8ca14d73
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-03 17:33:02 -0300

    feat: cria action show de movie_genres

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -2,4 +2,8 @@ class MovieGenresController < ApplicationController
   def index
     @movie_genres = MovieGenre.all
   end
+
+  def show
+    @movie_genre = MovieGenre.includes(:movies).find(params[:id])
+  end
 end
