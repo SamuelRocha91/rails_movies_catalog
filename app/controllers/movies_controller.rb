3558107521e43ca3f1cfc89a6a90711de165b6a8
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-03 02:27:36 -0300

    feat: adiciona e configura gem kaminari

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    page = params[:page] || 1 
+    @movies = Movie.page(page).per(10)  
     @movie_genres = MovieGenre.all 
   end
 end
