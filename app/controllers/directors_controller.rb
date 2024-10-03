cc4a0d2590fbe2e2055a79a4c48ed40fa1421934
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-03 20:46:07 -0300

    feat: cria action create e filtro de parâmetros em directors controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,6 +5,24 @@ class DirectorsController < ApplicationController
 
   def new
     @director = Director.new
+    @movie_genres = MovieGenre.all
   end
-  
+
+  def create
+     @director = Director.new(director_params)
+    if @director.save
+      flash[:notice] = "Director successfully created!"
+      redirect_to director_path(@director.id)
+    else
+      flash.now[:alert] = "There was an error creating the director."
+      render :new
+    end
+  end
+
+  private
+
+  def director_params
+    params.require(:director).permit(:name, :nationality, :birth_date, :favorite_genre_id)
+  end
+
 end
