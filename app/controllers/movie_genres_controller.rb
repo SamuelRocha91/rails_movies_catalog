7e2238012c8e1d7584d6411d4a6c746756b1e7e0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-03 18:02:29 -0300

    feat: cria action de update movie_genre

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -10,5 +10,21 @@ class MovieGenresController < ApplicationController
   def edit
     @movie_genre = MovieGenre.find(params[:id])
   end
-  
+
+  def update
+    @movie_genre = MovieGenre.find(params[:id])
+    if @movie_genre.update(movie_genre_params)
+      flash[:notice] = "Movie successfully updated!"
+      redirect_to movie_genre_path(@movie.id)
+    else
+      flash.now[:alert] = "There was an error creating the movie"
+      render :edit
+    end
+  end
+
+  private
+
+  def movie_genre_params
+    params.require(:movie_genre).permit(:name)
+  end
 end
