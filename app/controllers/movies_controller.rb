192c182f236f8c58df92d45139eac4d68dbfc618
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-03 09:49:37 -0300

    feat: refatora filtros e separa css em arquivos

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,16 @@
 class MoviesController < ApplicationController
   def index
-    page = params[:page] || 1 
-    @movies = Movie.page(page).per(10)  
+    page = params[:page] || 1
     @movie_genres = MovieGenre.all 
+    @movies = Movie.all
+    @movies = @movies
+      .where(
+        movie_genre_id: params[:category_id]
+      ) if params[:category_id].present?
+    @movies = @movies
+      .where(
+        'title LIKE ?', "%#{params[:movie_name]}%"
+      )  if params[:movie_name].present?
+    @movies = @movies.page(page).per(10)  
   end
 end
